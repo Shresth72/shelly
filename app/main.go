ae2dd849be2fbfe4795cb2e6ef3513b7cb24249c
Shresth72 <shashankshrestha539@gmail.com>
2026-05-04 18:41:40 +0000

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,27 +14,39 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("$ ")
-		command, err := reader.ReadString('\n')
+		commands, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading line:", err)
 			os.Exit(1)
 		}
 
-		if handleCommands(command) {
+		if handleCommands(commands) {
 			break
 		}
 	}
 
 }
 
-func handleCommands(command string) bool {
-	command = strings.TrimSpace(command)
+func handleCommands(commands string) bool {
+	commands = strings.TrimSpace(commands)
+	commandsArr := strings.Split(commands, " ")
+
+	command := commandsArr[0]
 
 	switch command {
 	case "exit":
 		return true
+	case "echo":
+		handleEcho(commandsArr)
 	default:
 		fmt.Printf("%s: command not found\n", command)
 		return false
 	}
+
+	return false
+}
+
+func handleEcho(commands []string) {
+	cmdStr := strings.Join(commands[1:], " ")
+	fmt.Printf("%s\n", cmdStr)
 }
