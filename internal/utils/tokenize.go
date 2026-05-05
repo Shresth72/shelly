98e31c64c4170b5c5cf2824c39595e3ba0acb06c
Shresth72 <shashankshrestha539@gmail.com>
2026-05-05 22:44:08 +0000

    codecrafters submit [skip ci]

--- internal/utils/tokenize.go
+++ internal/utils/tokenize.go
@@ -2,26 +2,50 @@ package utils
 
 import "strings"
 
+type quoteState int
+
+const (
+	none quoteState = iota
+	single
+	double
+	escape
+)
+
 func Tokenize(input string) []string {
 	var result []string
 	var current strings.Builder
 
-	inSingle := false
+	state := none
 
 	for i := 0; i < len(input); i++ {
 		ch := input[i]
 
 		switch ch {
 		case '\'':
-			inSingle = !inSingle
+			if state == none {
+				state = single
+			} else if state == single {
+				state = none
+			} else {
+				current.WriteByte(ch)
+			}
+
+		case '"':
+			if state == none {
+				state = double
+			} else if state == double {
+				state = none
+			} else {
+				current.WriteByte(ch)
+			}
 
 		case ' ':
-			if inSingle {
-				current.WriteByte(ch)
-			} else {
+			if state == none {
 				if current.Len() > 0 {
 					result = append(result, current.String())
 					current.Reset()
+				} else {
+					current.WriteByte(ch)
 				}
 			}
 
