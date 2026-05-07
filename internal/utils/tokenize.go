27ac29a68fa733b7e9f9080754048e6f828302a3
Shresth72 <shashankshrestha539@gmail.com>
2026-05-07 17:59:27 +0000

    codecrafters submit [skip ci]

--- internal/utils/tokenize.go
+++ internal/utils/tokenize.go
@@ -12,58 +12,61 @@ const (
 )
 
 func Tokenize(input string) []string {
-	var result []string
+	var tokens []string
 	var current strings.Builder
 
-	state := none
+	var quote byte
+	escaped := false
+
+	flush := func() {
+		if current.Len() > 0 {
+			tokens = append(tokens, current.String())
+			current.Reset()
+		}
+	}
 
 	for i := 0; i < len(input); i++ {
 		ch := input[i]
 
+		if escaped {
+			current.WriteByte(ch)
+			escaped = false
+			continue
+		}
+
 		switch ch {
-		case '\'':
-			if state == none {
-				state = single
-			} else if state == single {
-				state = none
+		case '\\':
+			escaped = true
+			if quote == 0 {
+				quote = ch
+			} else if quote == ch {
+				quote = 0
 			} else {
 				current.WriteByte(ch)
 			}
 
-		case '"':
-			if state == none {
-				state = double
-			} else if state == double {
-				state = none
+		case '"', '\'':
+			if quote == 0 {
+				quote = ch
+			} else if quote == ch {
+				quote = 0
 			} else {
 				current.WriteByte(ch)
 			}
 
-		case ' ':
-			if state == none {
-				if current.Len() > 0 {
-					result = append(result, current.String())
-					current.Reset()
-				}
-			} else {
+		case ' ', '\t', '\n':
+			if quote != 0 {
 				current.WriteByte(ch)
+			} else {
+				flush()
 			}
-
-		// case '\\':
-		// 	if state == double {
-		// 		if state != escape {
-		// 			state = escape
-		// 		} else if state
-		// 	}
 
 		default:
 			current.WriteByte(ch)
 		}
 	}
 
-	if current.Len() > 0 {
-		result = append(result, current.String())
-	}
+	flush()
 
-	return result
+	return tokens
 }
